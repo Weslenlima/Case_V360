5e53ad0762593e83da19d0e884424b41e55fc4a8
Weslen Lima <weslen.blima@gmail.com>
2025-10-17 23:19:47 -0300

    V1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_14_022444) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_14_013910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,7 +47,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_14_022444) do
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "due_date"
     t.index ["list_id"], name: "index_todos_on_list_id"
   end
 
