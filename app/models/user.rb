b1c5492391c0dafc53b0df0ba4ad52f2cb62141a
Weslen Lima <weslen.blima@gmail.com>
2025-10-18 02:14:34 -0300

    mensagem tela de cadastro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,30 @@
-class User < ApplicationRecord
+'''class User < ApplicationRecord
   has_secure_password
   has_many :lists, dependent: :destroy
 
-  validates :name, :email, presence: true
-  validates :email, uniqueness: true
+  # Validações diretas com mensagens em português
+  validates :name, presence: { message: "não pode ficar em branco" }
+  validates :email, presence: { message: "não pode ficar em branco" },
+                    uniqueness: { message: "já está em uso" }
+
+  validates :password, presence: { message: "não pode ficar em branco" },
+                       confirmation: { message: "e a confirmação devem ser iguais" }
+  validates :password_confirmation, presence: { message: "não pode ficar em branco" }
+end'''
+
+class User < ApplicationRecord
+  has_secure_password validations: false
+  has_many :lists, dependent: :destroy
+
+  validates :name, presence: { message: "Nome não pode ficar em branco" }
+  validates :email, presence: { message: "E-mail não pode ficar em branco" },
+                    uniqueness: { message: "E-mail já está em uso" }
+  validates :password, presence: { message: "Senha não pode ficar em branco" },
+                       confirmation: { message: "Senha e a confirmação devem ser iguais" }
+  validates :password_confirmation, presence: { message: "Confirmação não pode ficar em branco" }
+
+  # Sobrescreve full_messages para exibir apenas a mensagem
+  def errors_full_messages
+    errors.map(&:message)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module V360Case
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+   
+
+
+
   end
 end
